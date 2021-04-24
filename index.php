272034bf06a9cca1c13c766c3a7876e79da7ebcf
rgg7888 <rgg7888@outlook.com>
2021-04-23 19:47:50 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+dt();
+lmh([
+    head([
+        metaGroup("Free html system","html, php, css, javascript","Ramiro G"),
+        title("Http Requests & JavaScript"),
+        lnk("rstylesheet|happ.css"),
+        script("sxhr.js|d")
+    ]),
+    body([
+        section([
+            button("GET Data","iget-btn"),
+            button("POST Data","ipost-btn")
+        ],"icontrol-center")
+    ])
+],'en');
